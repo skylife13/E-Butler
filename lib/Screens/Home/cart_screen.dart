e180847eaa6d124db4abac1b71096c9aa4886d6c
skylife13 <benedick.clemons@binus.ac.id>
2021-10-25 16:03:49 +0700

    baru

--- lib/Screens/Home/cart_screen.dart
+++ lib/Screens/Home/cart_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ebutler/Model/user.dart';
+import 'package:ebutler/Services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '/providers/cart.dart' show Cart;
@@ -12,64 +14,85 @@ class CartScreen extends StatelessWidget {
   const CartScreen({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     final cart = Provider.of<Cart>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            margin: const EdgeInsets.all(15),
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return StreamBuilder<UserData>(
+        stream: null,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
+
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Cart'),
+              ),
+              body: Column(
                 children: <Widget>[
-                  const Text(
-                    'Total',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const Spacer(),
-                  Chip(
-                    label: Text(
-                      'Rp. ${cart.totalAmount.toStringAsFixed(2)}',
-                      style: const TextStyle(color: Colors.white),
+                  Card(
+                    margin: const EdgeInsets.all(15),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          const Text(
+                            'Total',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          const Spacer(),
+                          Chip(
+                            label: Text(
+                              'Rp. ${cart.totalAmount.toStringAsFixed(2)}',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            backgroundColor: Theme.of(context).primaryColor,
+                          ),
+                          TextButton(
+                            child: Text('Order Now',
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                )),
+                            onPressed: () async {
+                              await DatabaseService(uid: user.uid)
+                                  .updateUserData(
+                                userData.name,
+                                cart.items.values ?? userData.cola,
+                                cart.items.values ?? userData.pillow,
+                                cart.items.values ?? userData.towel,
+                                cart.items.values ?? userData.mineralwater,
+                              );
+                              Provider.of<Orders>(context, listen: false)
+                                  .addOrder(
+                                cart.items.values.toList(),
+                                cart.totalAmount,
+                              );
+                              cart.clear();
+                              Navigator.of(context)
+                                  .pushNamed(OrderScreen.routeName);
+                            },
+                          )
+                        ],
+                      ),
                     ),
-                    backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  TextButton(
-                    child: Text('Order Now',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                        )),
-                    onPressed: () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear();
-                      Navigator.of(context).pushNamed(OrderScreen.routeName);
-                    },
-                  )
+                  const SizedBox(height: 10),
+                  Expanded(
+                      child: ListView.builder(
+                    itemBuilder: (ctx, i) => CartItem(
+                        productId: cart.items.keys.toList()[i],
+                        id: cart.items.values.toList()[i].id,
+                        title: cart.items.values.toList()[i].title,
+                        quantity: cart.items.values.toList()[i].quantity,
+                        price: cart.items.values.toList()[i].price),
+                    itemCount: cart.itemCount,
+                  ))
                 ],
               ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-              child: ListView.builder(
-            itemBuilder: (ctx, i) => CartItem(
-                productId: cart.items.keys.toList()[i],
-                id: cart.items.values.toList()[i].id,
-                title: cart.items.values.toList()[i].title,
-                quantity: cart.items.values.toList()[i].quantity,
-                price: cart.items.values.toList()[i].price),
-            itemCount: cart.itemCount,
-          ))
-        ],
-      ),
-    );
+            );
+          } else {
+            return null;
+          }
+        });
   }
 }
