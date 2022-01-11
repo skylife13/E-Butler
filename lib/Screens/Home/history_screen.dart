7974e791d01428cbe1923ec33a9a9971af68b319
skylife13 <benedick.clemons@binus.ac.id>
2022-01-11 12:39:52 +0700

    history baru

--- lib/Screens/Home/history_screen.dart
+++ lib/Screens/Home/history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ebutler/Screens/Notifications/components/default_appbar.dart';
 import 'package:ebutler/Screens/Notifications/components/default_backbutton.dart';
+import 'package:ebutler/Shared/constants.dart';
 import 'package:intl/intl.dart';
 
 import '/Model/user.dart';
@@ -24,10 +25,11 @@ class _HistoryScreenState extends State<HistoryScreen> {
     final user = Provider.of<User>(context);
 
     String uid = user.uid;
-
+    int quantity;
     Timestamp newTime;
     DateTime newDate;
     String time;
+    String title;
     int roomNumber;
 
     var streamSnapshot = Firestore.instance.collection(uid).snapshots();
@@ -44,8 +46,10 @@ class _HistoryScreenState extends State<HistoryScreen> {
               child: Text('Kosong'),
             );
           }
+          title = '';
           newDate = null;
           time = '';
+          quantity = null;
           roomNumber = null;
           return ListView(
             children: snapshot.data.documents.map(
@@ -53,20 +57,67 @@ class _HistoryScreenState extends State<HistoryScreen> {
                 for (var i in doc.data.values) {
                   roomNumber = i['Room Number'];
                   newTime = i['Time'];
+                  title = i['Item'];
+                  quantity = i['Quantity'];
                   newDate = newTime.toDate();
                   time = DateFormat('dd/MM/yyyy - kk:mm:ss').format(newDate);
                   break;
                 }
-                return Column(
-                  children: [
-                    for (var i in doc.data.values)
-                      Text(
-                        i['Item'].toString(),
+                return Container(
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 4,
+                    ),
+                    elevation: 8,
+                    shadowColor: Colors.black38,
+                    child: ListTile(
+                      leading: Text(
+                        time,
+                        style: TextStyle(fontSize: 10),
                       ),
-                    Text(roomNumber.toString()),
-                    Text(time),
-                  ],
+                      title: Container(
+                          width: 300,
+                          child: Text('Room No. ' + roomNumber.toString(),
+                              style: TextStyle(fontSize: 12))),
+
+                      trailing: Column(
+                        children: [
+                          for (var i in doc.data.values)
+                            Text(
+                              i['Item'].toString() + ' $quantity x',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                        ],
+                      ),
+
+                      // children: [
+                      //   const Text('Item: '),
+                      //   for (var i in doc.data.values)
+                      //     Text(i['Item'].toString() + ' $quantity x\n'),
+                      //   Text('Room Number: ' + roomNumber.toString()),
+                      //   Text('Date: ' + time + '\n'),
+                      // ],
+                    ),
+                  ),
                 );
+                // return ListTile(
+                //   // leading: Column(
+                //   children: [
+                //     for (var i in doc.data.values)
+                //     Text(i['Item'].toString()),
+                //     Text(roomNumber.toString()),
+                //     Text(time),
+                //   ],
+                //   // ),
+                //   title: Text('Item: ' + title),
+                //   subtitle: Text('Quantity: $quantity x'),
+                //   trailing: Text('Room Number: ' +
+                //       roomNumber.toString() +
+                //       '\n' +
+                //       'Date:' +
+                //       time),
+                // );
               },
             ).toList(),
           );
